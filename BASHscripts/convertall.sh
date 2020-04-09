2ea35a5be68d87bcfa874cb33d3eb4c753f3a084
Benjamin147 <49642244+Benjamin147@users.noreply.github.com>
2020-04-09 17:45:09 +0200

    Update convertall.sh

--- BASHscripts/convertall.sh
+++ BASHscripts/convertall.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash 
 
+# which prefix u want to convert to
 old='png'
+# new data format for all of this files
 new='png'
 backupfolder='backupfolder'
 mkdir '../'$backupfolder
@@ -18,7 +20,9 @@ do
    echo '('$counter' of '$num') '$file
    newfile=${file::-4}'.'$new
    cp $file '../'$backupfolder
-   convert -density 300 -resize 1024 $file $newfile
+   convert -density 300 -resize 1024 $file $newfile 
+   # <--- Stell die Auflösung hier ein, die 1024 sind die x-Achse des Bildes, y wird dann entsprechend gesetzt
+   # -density ist hier die dpi
    #mv $file $backupfolder
    counter=$((counter+1))
 done
